5f3dba781fc4ab25501600dca8690864b623e331
linaverde <p.koroleva1102@gmail.com>
2018-11-25 15:53:18 +1000

    fix reading and printing

--- src/reading_lib.cpp
+++ src/reading_lib.cpp
@@ -7,18 +7,20 @@
 char **readFile(const char *filepath) {
     int len, count;
     FILE *f = fopen(filepath, "r");
-    if (f != NULL){
-        fscanf(f, "%d\t%d\n", &len, &count);
-        char **str = new char* [count];
-        str[0] = new char [len];
-        str[0][0] = (char) len; //давайте надеяться
-        str[0][1] = (char) count; //что оно туда поместится
-        for (int i = 1; i <= count; i++){
-            str[i] = new char [len];
-            fscanf(f, "%s", str[i]);
-        }
-        fclose(f);
-        return str;
+    if (f == NULL){
+        return nullptr;
     }
-    return nullptr;
+    fscanf(f, "%d\t%d\n", &len, &count);
+    char **str = new char* [count + 1];
+    int n = sizeof(int)*2;
+    str[0] = new char[n];
+    ((int*) str)[0] = len;
+    ((int*) str)[1] = count;
+    for (int i = 1; i <= count; i++){
+        str[i] = new char [len];
+        fscanf(f, "%s", str[i]);
+    }
+    fclose(f);
+    return str;
+
 }
